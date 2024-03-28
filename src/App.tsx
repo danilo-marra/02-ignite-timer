fa9a2c3a637f46ed5291f6364bb82de9532ce46b
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-03-28 14:56:53 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Home } from './pages/Home'
+import { Router } from './Router'
 
 export function App() {
   return (
-   <>
-   <Button variant="primary"/>
-   <Button variant="secondary"/>
-   <Button variant="success"/>
-   <Button variant="danger"/>
-   <Button/>
-   </>
+    <ThemeProvider theme={defaultTheme}>
+      {' '}
+      {/* context providers */}
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-
